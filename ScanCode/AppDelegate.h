52ad300f6bf2defebd589bc9605d17658362ab8d
ZivDo <dzctony@163.com>
2013-04-09 23:45:55 +0800

    Initial Commit

---
+++ ScanCode/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ScanCode
+//
+//  Created by Do Ziv on 13-4-9.
+//  Copyright (c) 2013年 Do Ziv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ScanCode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScanCode
+//
+//  Created by Do Ziv on 13-4-9.
+//  Copyright (c) 2013年 Do Ziv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
